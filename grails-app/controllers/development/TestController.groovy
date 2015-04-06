75df25e66fbbfd6e4619df674a4462c1f369e0fa
richard <richard.holaj@iaeste.cz>
2015-04-06 22:14:21 +0200

    Uncommented parts of TestController

--- grails-app/controllers/development/TestController.groovy
+++ grails-app/controllers/development/TestController.groovy
@@ -3,19 +3,19 @@ package development
 class TestController {
 
     def index() {
-        /*println Test.findAllByNumberBetween(1,12)
-        def t = Test.get(1) ?: new Test(name:"qwbaaadsyxabc",number: 10, test:"nope")*/
+        println Test.findAllByNumberBetween(1,12)
+        def t = Test.get(1) ?: new Test(name:"qwbaaadsyxabc",number: 10, test:"nope")
         new RemoteTest(otherName: "TEST").save()
         new RemoteTest(otherName: "TEST").save()
-        /*t.save()
+        t.save()
         new RemoteTest(otherName: "TEST2").save()
         println Test.list(sort: "id", order: 'asc')*.name
         t.name = "AHOJ2"
         t.save()
-        t = Test.get(2)*/
+        t = Test.get(2)*
         println Test.list(sort: "id", order: 'asc')*.name
-        /*println Test.list(sort: "id", order: 'asc')*.name
+        println Test.list(sort: "id", order: 'asc')*.name
         t?.delete()
-        println Test.list(sort: 'id', order: 'asc')*.name*/
+        println Test.list(sort: 'id', order: 'asc')*.name
     }
 }
